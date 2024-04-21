0898036b149bc52669ccb82e81ef0649995c5a4d
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-04-21 13:01:01 +0200

    Update IDisplayable.cs
    this interface will be placing animals on a Cavnas

--- IDisplayable.cs
+++ IDisplayable.cs
@@ -1,1 +1,11 @@
+using System.Windows.Controls;
 
+namespace PreyPredator.Contracts
+{
+    public interface IDisplayable
+    {
+        void DisplayOn(Canvas canvas);
+        void StopDisplaying();
+        void UpdateDisplay();
+    }
+}
